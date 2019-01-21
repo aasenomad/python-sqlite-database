0a070873af387c8f5bd2aaf36a2b4f306cee65f8
Jonathan Hrang <jsianguk@gmail.com>
2019-01-21 16:46:18 -0500

    first commit

---
+++ employee.py
@@ -0,0 +1,15 @@
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+    @property
+    def email(self):
+        return '{},{}@email.com'.format(self.first.last)
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def __repr__(self):
+        return "Employee('{}','{}', {})".format(self.first, self.last, self.pay)    
